13bcc3cdf7728ed334955ca08c66f66f80530638
Matt Harnett <harnettmatt@gmail.com>
2023-07-16 18:03:02 -0700

    v1 of app

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,81 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="intro">
+    <h1>Welcome to ThemeSong! </h1>
+    <h4>ThemeSong is designed for Strava athletes that listen to music while exercising. Log an activity and ThemeSong
+      will determine which song got you the most fired up and post it in the activity
+      description.</h4>
+  </div>
+  <div class="buttons-container">
+    <div class="step">
+      Step 1
+      <button class="step-content">
+        <img id="strava" src="@/assets/strava/connect_with_strava.png" />
+      </button>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="step">
+      Step 2
+      <button class="step-content">
+        <img id="spotify" src="@/assets/spotify/connect_with_spotify.png" />
+      </button>
+    </div>
+  </div>
+  <img id="powered-by-strava" src="@/assets/strava/powered_by_strava.png">
 </template>
 
-<style scoped>
-header {
+<style>
+html,
+body {
+  background-color: rgb(73, 73, 73);
+  margin: 0;
+  padding: 5px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
   line-height: 1.5;
-  max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.intro {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: justify;
+  max-width: 800px;
+  margin: 0 auto;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.buttons-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 25vh;
+  color: white;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
+.step-content {
   display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  margin: 20px;
+  background-color: transparent;
+  border: none;
 }
 
-nav a:first-of-type {
-  border: 0;
+.step {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+#spotify {
+  width: 193px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#powered-by-strava {
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
