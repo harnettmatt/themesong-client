5c37ffd56e7d96c6cfda315bf983e09d3e7e1bfb
Matt Harnett <harnettmatt@gmail.com>
2023-07-17 18:28:47 -0700

    link to strava works to retrieve user id + formatting changes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute()
+const userId = computed(() => {
+  return route.params.userId
+})
+
 </script>
 
 <template>
-  <div class="intro">
+  <div id="app" class="intro">
     <h1>Welcome to ThemeSong! </h1>
     <h4>ThemeSong is designed for Strava athletes that listen to music while exercising. Log an activity and ThemeSong
       will determine which song got you the most fired up and post it in the activity
@@ -11,17 +18,21 @@
   <div class="buttons-container">
     <div class="step">
       Step 1
-      <a href="http://localhost:8000/strava/login">
-        <button class="step-content">
-          <img id="strava" src="@/assets/strava/connect_with_strava.png" />
-        </button>
+      <a href="http://localhost:8000/strava/login" class="link">
+        <img id="strava" class="step-content" src="@/assets/strava/connect_with_strava.png" />
       </a>
+      <div class="checkmark-container">
+        <img class="checkmark" src="@/assets/checkmark.png" />
+      </div>
     </div>
     <div class="step">
       Step 2
-      <button class="step-content">
-        <img id="spotify" src="@/assets/spotify/connect_with_spotify.png" />
-      </button>
+      <a href="http://localhost:8000/strava/login" class="link">
+        <img id="spotify" class="step-content" src="@/assets/spotify/connect_with_spotify.png" />
+      </a>
+      <div class="checkmark-container">
+        <img class="checkmark" src="@/assets/checkmark.png" />
+      </div>
     </div>
   </div>
   <img id="powered-by-strava" src="@/assets/strava/powered_by_strava.png" />
@@ -47,6 +58,13 @@ body {
   margin: 0 auto;
 }
 
+.step-content {
+  display: inline-block;
+  margin: 20px;
+  background-color: transparent;
+  border: none;
+}
+
 .buttons-container {
   display: flex;
   flex-direction: column;
@@ -54,13 +72,6 @@ body {
   align-items: center;
   height: 25vh;
   color: white;
-}
-
-.step-content {
-  display: inline-block;
-  margin: 20px;
-  background-color: transparent;
-  border: none;
 }
 
 .step {
@@ -72,6 +83,24 @@ body {
 
 #spotify {
   width: 193px;
+}
+
+.checkmark {
+  display: inline-block;
+  height: 20px;
+}
+
+/* Add a container for the check mark */
+.checkmark-container {
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.link {
+  text-decoration: none;
+  display: inline-block;
+  line-height: 0;
 }
 
 #powered-by-strava {
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
-import { createApp } from 'vue'
-
 import App from './App.vue'
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
+// Define your routes
+const routes = [
+    {
+        path: '/',
+        name: 'ThemeSong',
+        component: App
+    },
+    {
+        path: '/:userId',
+        name: 'ThemeSong',
+        component: App
+    }
+]
+
+// Create the router instance
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+// Create your Vue app
 const app = createApp(App)
-
+app.use(router)
 app.mount('#app')
